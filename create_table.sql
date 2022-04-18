fbc578f6a89a076b0ea285e583012556fc584116
fu10ta <keita_fuchigami@fu10ta.com>
2022-04-18 12:30:22 +0900

    create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE jira_table (
+    `id` INT NOT NULL AUTO_INCREMENT UNSIGNED,
+    `date` DATE NOT NULL,
+    `total` INT UNSIGNED,
+    `backlog` INT UNSIGNED,
+    `in-progress` INT UNSIGNED,
+    `in-check` INT UNSIGNED,
+    `on-review` INT UNSIGNED,
+    `done` INT UNSIGNED,
+    `total-progress-rate` FLOAT UNSIGNED,
+    `eng-progress-rate` FLOAT UNSIGNED,
+    `date-to-all-in` INT UNSIGNED,
+    `outstanding` INT UNSIGNED,
+    `wip` INT UNSIGNED,
+    `ideal-progress-rate` FLOAT UNSIGNED,
+    `num-testers` INT UNSIGNED,
+    `additional-testers` INT UNSIGNED,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` DATETIME
+);
