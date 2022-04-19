8e87bcf6794543995e64e8d5b4c7d72a47e2230a
airi-kinjo-rh <airi_kinjo@reazon.jp>
2022-04-19 15:53:41 +0900

    add getCountsByReleaseId

--- backend/app.ts
+++ backend/app.ts
@@ -15,6 +15,7 @@ const connection = mysql.createConnection({
     database: 'jira_db'
   });
 
+/*
 const getMonthData = (req : express.Request, res : express.Response) => {
     connection.query(
         `SELECT * FROM jira_table WHERE date >= '${req.query['target_month']}-01' AND date <= '${req.query['target_month']}-31'`,
@@ -25,10 +26,11 @@ const getMonthData = (req : express.Request, res : express.Response) => {
         }
       );  
 }
+*/
 
 const getReleases = (req : express.Request, res : express.Response) => {
   connection.query(
-      `SELECT id, name, start_date, end_date FROM releases WHERE deleted_at IS NULL`,
+      `SELECT * FROM releases WHERE deleted_at IS NULL`,
       function(err, results, fields) {
         console.log(results); // results contains rows returned by server
         console.log(fields); // fields contains extra meta data about results, if available
@@ -37,9 +39,16 @@ const getReleases = (req : express.Request, res : express.Response) => {
     );  
 }
 
+const getCountsByReleaseId = (req : express.Request, res : express.Response) => {
+  connection.query(
+      `SELECT * FROM counts WHERE release_id = ${req.query['release_id']} AND deleted_at IS NULL`,
+      function(err, results, fields) {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.json(results);
+      }
+    );    
+}
 
-
-
-app.get('/', getMonthData);
 app.get('/getReleases', getReleases);
-// app.get('/getCountsByReleaseId?release_id=${id}', getCountsByReleaseId);
+app.get('/getCountsByReleaseId', getCountsByReleaseId);
